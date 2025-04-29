4f47515973623e065a64a11fbd8733e8c40dd17f
Mirror <yllikano@gmail.com>
2025-04-29 10:11:12 +0300

    feat: add GenreList component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { JSX } from 'react';
 
+import { GenreList } from '@/components/GenreList';
 import { PromoFilm } from '@/components/PromoFilm';
 
 export default function HomePage(): JSX.Element {
@@ -10,6 +11,8 @@ export default function HomePage(): JSX.Element {
       <div className="page-content">
         <section className="catalog">
           <h2 className="catalog__title visually-hidden">Catalog</h2>
+
+          <GenreList />
         </section>
       </div>
     </>
