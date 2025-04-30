766d74ac108980082cafceff202255bddd685f8c
Mirror <yllikano@gmail.com>
2025-04-30 06:45:33 +0300

    feat: add UserBlock component and ref Header logic

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,28 +1,50 @@
-import Image from 'next/image';
+import Link from 'next/link';
 import { JSX } from 'react';
 
-export default function Header(): JSX.Element {
+import UserBlock from '../UserBlock';
+
+interface HeaderProps {
+  pageTitle?: string;
+  showBreadcrumbs?: boolean;
+  filmTitle?: string;
+  filmId?: string;
+}
+
+export default function Header({
+  pageTitle,
+  showBreadcrumbs,
+  filmTitle,
+  filmId,
+}: HeaderProps): JSX.Element {
   return (
     <header className="page-header user-page__head">
       <div className="page-header__container">
         <div className="logo">
-          <a href="main.html" className="logo__link">
+          <Link href="/" className="logo__link">
             <span className="logo__letter logo__letter--1">W</span>
             <span className="logo__letter logo__letter--2">T</span>
             <span className="logo__letter logo__letter--3">W</span>
-          </a>
+          </Link>
         </div>
 
-        <ul className="user-block">
-          <li className="user-block__item">
-            <div className="user-block__avatar">
-              <Image src="/img/avatar.jpg" alt="User avatar" width={63} height={63} />
-            </div>
-          </li>
-          <li className="user-block__item">
-            <a className="user-block__link">Sign out</a>
-          </li>
-        </ul>
+        {pageTitle && <h1 className="page-title user-page__title">{pageTitle}</h1>}
+
+        {showBreadcrumbs && filmTitle && filmId && (
+          <nav className="breadcrumbs">
+            <ul className="breadcrumbs__list">
+              <li className="breadcrumbs__item">
+                <Link href={`/films/${filmId}`} className="breadcrumbs__link">
+                  {filmTitle}
+                </Link>
+              </li>
+              <li className="breadcrumbs__item">
+                <span className="breadcrumbs__link">{pageTitle}</span>
+              </li>
+            </ul>
+          </nav>
+        )}
+
+        <UserBlock isAuth={false} />
       </div>
     </header>
   );
