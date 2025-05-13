8d634780b5d66b8abd302733ed31bf69364f5f41
Mirror <yllikano@gmail.com>
2025-05-13 23:56:43 +0300

    fix: next-config domains

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['16.design.htmlacademy.pro'], // добавляем домен
+    domains: ['16.design.htmlacademy.pro'],
   },
 };
 
